c5070aca2cd97f1500371805309b27c195431885
Anton Ödman <anton.odman@gmail.com>
2019-10-25 16:40:44 +0200

    Added cover control over color buttons

--- src/client/components/Color.js
+++ src/client/components/Color.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import useHover from 'react-use-hover';
+
+const ColorClass = require('color');
 
 
 const Color = ({ color }) => {
+  const [isHovering, hoverProps] = useHover();
+  // get class for color
+  const colorClass = ColorClass(color);
+
   // override CSS color
   const style = {
-    backgroundColor: color
+    backgroundColor: color,
   };
 
+  // override CSS when hovering
+  const hoverStyle = {
+    backgroundColor: colorClass.darken(0.7)
+  };
+
+
   return (
-    <div role="presentation" onClick={() => console.log(color)} style={style} className="color" />
+    <div role="presentation" {...hoverProps} onClick={() => console.log(color)} style={isHovering ? hoverStyle : style} className="color">
+      {isHovering ? <p id="in-color-text">COPY</p> : null}
+    </div>
   );
 };
 
